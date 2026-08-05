86933b7b1e9d17765522238823d1d326061a0294
SambhavSharma7781 <sambhav7717@gmail.com>
2026-08-05 22:16:21 +0530

    Replace logo image with new swagdrive logo and update Logo component to use img tag. Add swagdrive-logo.png to public assets.

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 
@@ -12,20 +11,22 @@ export function Logo({ className }: LogoProps) {
       href="/"
       aria-label="Swag Drive home"
       className={cn(
-        "inline-flex shrink-0 items-center rounded-md outline-none",
+        "inline-flex shrink-0 items-center outline-none",
         "transition-opacity duration-[250ms] ease-[cubic-bezier(0.22,1,0.36,1)]",
         "hover:opacity-75 focus-visible:ring-2 focus-visible:ring-brand-navy/30 focus-visible:ring-offset-2",
         "active:opacity-60",
         className
       )}
     >
-      <Image
-        src="/logo.png"
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/swagdrive-logo.png"
         alt="Swag Drive"
-        width={360}
-        height={80}
-        priority
-        className="h-[3.25rem] w-auto max-w-[min(100%,11rem)] object-contain object-left sm:h-14 sm:max-w-none lg:h-[4.25rem]"
+        width={924}
+        height={172}
+        decoding="async"
+        fetchPriority="high"
+        className="h-8 w-auto max-w-[11rem] object-contain object-left sm:h-9 sm:max-w-[12.5rem] lg:h-10 lg:max-w-[14rem]"
       />
     </Link>
   );
